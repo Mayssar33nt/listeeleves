d34ba0f579cf52a6ada855e534a958cc9b83caa2
Mayssar33nt <mayssar33prod@gmail.com>
2025-08-27 21:55:06 +0100

    تطبيق خط Cairo على جميع التطبيق

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Cairo', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        'cairo': ['Cairo', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
